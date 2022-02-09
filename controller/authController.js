0b8a789147a5d3e96d6b109bba019a3980502dcd
Dradwing <Rushbuket@gmail.com>
2022-02-09 15:50:02 +0530

    adding authcontroller.js

---
+++ controller/authController.js
@@ -0,0 +1,22 @@
+const jwt=require('jsonwebtoken');
+
+
+
+const createSendToken=async(user,status,res)=>{
+
+    const cookieOptions={
+        //browser will automatically delte it after expires
+        expires: new Date(
+          Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+      };
+      if(process.env.ENV=="production")cookieOptions.secure=true;
+      res.cookie(jwt,process.env.)
+
+
+      white 
+      black
+      
+
+}
